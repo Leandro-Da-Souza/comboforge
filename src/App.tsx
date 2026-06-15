bf1d8a412dbb3da0464582a1e48f14e230eb84f5
Leandro Da Souza <dasouza.leandro@gmail.com>
2026-06-15 09:02:16 +0200

    Add react router to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Navigate, Route, Routes } from 'react-router'
 import Train from './components/Train'
 
 function App() {
-  return <Train />
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/train" replace />} />
+      <Route path="/train" element={<Train />} />
+      <Route path="/combos" element={<div>Combos</div>} />
+      <Route path="/history" element={<div>History</div>} />
+      <Route path="/settings" element={<div>Settings</div>} />
+    </Routes>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './styles/reset.css'
 import './styles/theme.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
